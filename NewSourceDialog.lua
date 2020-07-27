6b0eff7cff79cdb83e00e1200f092b6fb06ec2f1
pirogronian <pirogronian0@gmail.com>
2020-07-27 21:26:43 +0200

    Improve NewSourceDialog layout.

---
+++ NewSourceDialog.lua
@@ -0,0 +1,32 @@
+
+local function NewSourceDialog(parentId)
+    local closed = false;
+    local id = nil;
+    if Slab.BeginDialog("NewSourceDialog", { Title = "Create new source from file", AllowResize = true }) then
+        Slab.BeginLayout("NewSourceDialogLayout", { Columns = 2 });
+        Slab.SetLayoutColumn(1);
+        Slab.Text("Parent object:");
+        Slab.Text("Id:");
+        Slab.SetLayoutColumn(2);
+        Slab.Text(parentId);
+        Slab.Input("SourceId", { ReturnOnText = false });
+        id = Slab.GetInputText();
+        Slab.EndLayout();
+        Slab.BeginLayout("NewSourceDialogButtonsLayout", { Columns = 2, AlignX = "center" });
+        if Slab.Button("Create") then
+            Slab.CloseDialog("NewSourceDialog");
+            closed = true;
+        end
+        Slab.SameLine();
+        if Slab.Button("Cancel") then
+            Slab.CloseDialog("NewSourceDialog");
+            closed = true;
+            id = nil;
+        end
+        Slab.EndLayout();
+        Slab.EndDialog();
+    end
+    return closed, id;
+end
+
+return NewSourceDialog;
