4f05bfefb4e1aaaaa41121a889acd9d14acd73b6
pirogronian <pirogronian0@gmail.com>
2020-08-02 15:35:00 +0200

    Saving playback position (not always).

--- SourceItem.lua
+++ SourceItem.lua
@@ -5,13 +5,24 @@ local SItem = Item:subclass("SourceItem");
 
 function SItem:initialize(data, parent)
     local id = data;
+    local playPos = 0;
     if type(data) == 'table' then
         id = data.id;
         parent = data.parent;
+        playPos = data.source.playPos;
     end
     Item.initialize(self, id, parent);
     self.attributes = { name = id };
     self.source = love.audio.newSource(self.parent.id, "static");
+    self.source:seek(playPos);
+end
+
+function SItem:getSerializableData()
+    local data = Item.getSerializableData(self);
+    local sdata = {};
+    sdata.playPos = self.source:tell();
+    data.source = sdata;
+    return data;
 end
 
 return SItem;
